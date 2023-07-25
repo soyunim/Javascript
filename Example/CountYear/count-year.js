37af1863c3c825677049d78185bc5ed28d285151
soyunim <togksk123@naver.com>
2023-07-26 00:52:39 +0900

    230726

---
+++ Example/CountYear/count-year.js
@@ -0,0 +1,8 @@
+var currentYear = 2023;
+var birthYear;
+var age;
+
+birthYear = prompt("태어난 연도를 입력하세요. (YYYY)");
+age = currentYear - birthYear;
+document.write(currentYear + "년 현재<br>");
+document.write(birthYear + "년에 태어난 사람의 나이는 " + age + "세 입니다.");
