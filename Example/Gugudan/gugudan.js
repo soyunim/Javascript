f3917c10aed252945d071ce70b57b050dc99a116
soyunim <togksk123@naver.com>
2023-07-27 15:02:38 +0900

    230727

---
+++ Example/Gugudan/gugudan.js
@@ -0,0 +1,10 @@
+var i, j;
+
+for(i=1; i<=9; i++){
+    document.write("<div>");
+    document.write("<h3>" + i + "단</h3>");
+    for(j=1; j<=9; j++){
+        document.write(i + "X" + j + "=" + i*j + "<br>");
+    }
+    document.write("</div>");
+}
